c09a47c00ea728cc972c5efaf5ff569e6662a334
mercy.waeni <mercy.waeni@calltronix.com>
2022-01-23 16:08:20 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 10 );
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max( 12,11,20,5 );
